16ffc3a6c5ab8ce3b2bd85d19e0a1aa0e0f22f16
Kristine Martin <kristinenicolemartin@gmail.com>
2018-04-13 20:34:36 -0700

    updated NODE folder and added review for node-express-sql

--- NODE/29-ZombieChallenge/Unsolved/zombiegame.js
+++ NODE/29-ZombieChallenge/Unsolved/zombiegame.js
@@ -14,3 +14,67 @@
 // Major Warning: inquirer's prompt function is "asynchronous", which means that the majority of your game logic will need to be inside the .then() function for your propmt. 
 
 // ===========================================================================================================
+var inquirer = require('inquirer');
+
+//set the initial health amounts
+var playerHealth = 100;
+var zombieHealth = 30;
+
+
+
+//attack function
+//random number generator
+//check if still alive
+
+function playRound() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "userGuess",
+            message: "Try to stay alive! Guess a number between [1-5]",
+            choices: ["1", "2", "3", "4", "5"]
+        }
+    ]).then(function(guess) {
+        if ( playerHealth > 0 || zombieHealth > 0 ) {
+
+            var damage = Math.floor(Math.random() * 5 ) + 1;
+
+            var zombieNum = Math.floor(Math.random() * 5) + 1;
+            console.log("The Zombie guessed " + zombieNum);
+
+            if ( zombieNum === parseInt(guess.userGuess)) {
+                
+                zombieHealth -= damage;
+
+                console.log('YOU HIT THE ZOMBIE WITH ' + damage + ' DAMAGE!!');
+                console.log("You have " + playerHealth + " health left. \nThe zombie has " + zombieHealth + " left.");
+
+                checkRound();
+            }
+            else {
+                playerHealth -= damage;
+                console.log('YOU GOT HIT WITH ' + damage + 'damage');
+                console.log("You have " + playerHealth + " health left. \nThe zombie has " + zombieHealth + " left.");
+
+                checkRound();
+            }
+        }
+    });
+}
+
+function checkRound() {
+    if (playerHealth <= 0) {
+        console.log("you are dead");
+
+        process.exit();
+    }
+
+    if (zombieHealth <= 0) {
+        console.log('zombie is re-dead');
+
+        process.exit();
+    }
+
+    playRound();
+}
+playRound();
